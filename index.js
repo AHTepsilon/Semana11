7c707f7b6e8ac522af666fecc8d7c27632501e86
AHTepsilon <alehertoro725@gmail.com>
2021-11-05 15:42:17 -0500

    added html, css and js

---
+++ index.js
@@ -0,0 +1,9 @@
+const userName = document.getElementById("username");
+const publication = document.getElementById("description");
+
+const submitBtn = document.getElementById("publishButton");
+
+submitBtn.addEventListener("click", (e, ev) =>
+{
+    alert("hello world");
+});
